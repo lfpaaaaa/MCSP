9c68eb22f127e50bd54bb8b3e0f6d318b94d7ee4
XINYIHUANG1 <xinyihuang1@student.unimelb.edu.au>
2026-09-18 14:21:54 +1000

    Update: change the  match colours

--- app/src/main/java/au/edu/unimelb/campuscompanion/ui/theme/Color.kt
+++ app/src/main/java/au/edu/unimelb/campuscompanion/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package au.edu.unimelb.campuscompanion.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val CampusTeal = Color(0xFF275E64)
-val CampusGreen = Color(0xFF4E6B3C)
-val CampusAmber = Color(0xFF8A5A12)
-val CampusBlue = Color(0xFF315F93)
-val CampusRed = Color(0xFF9B3D33)
-val CampusSurface = Color(0xFFFAFCFC)
-val CampusSurfaceDark = Color(0xFF101414)
+val CampusBlue = Color(0xFF005FAF)
+val CampusBlueGrey = Color(0xFF50606F)
+val CampusAmber = Color(0xFF705C00)
+val CampusRed = Color(0xFFBA1A1A)
+val CampusSurface = Color(0xFFF8FAFF)
+val CampusSurfaceDark = Color(0xFF111318)
